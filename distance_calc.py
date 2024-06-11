3be8cb284446cfd5ba7f19f3d78dfe88395ab625
Ryan Low <ryan.cheeyang.low@gmail.com>
2024-06-11 23:53:26 +0800

    fix st format

--- distance_calc.py
+++ distance_calc.py
@@ -1,33 +1,36 @@
+import streamlit as st
 import googlemaps
-from datetime import datetime 
+from datetime import datetime
 
+# Google Maps API key
 gmaps = googlemaps.Client(key='AIzaSyBygqJSsYWT_z2G4o-KAU6F_5hyGTI7jY0')
 
-start = input("Where are you starting your journey? ")
-goal = input("Where would you like to go? ")
-time_leaving_input = input("When will you be leaving? (HH:MM) Skip if you are leaving now ")
+# Streamlit interface
+st.title("Walking Directions Finder")
 
-if time_leaving_input:
-    current_date = datetime.now()
-    hour, minute = time_leaving_input.split(":")
-    time_leaving = datetime(current_date.year, current_date.month, current_date.day, int(hour), int(minute)).timestamp()
-else:
-    time_leaving = datetime.now().timestamp()
+start = st.text_input("Where are you starting your journey?")
+goal = st.text_input("Where would you like to go?")
+time_leaving_input = st.text_input("When will you be leaving? (HH\:MM) Leave blank if you are leaving now")
 
-directions_result = gmaps.directions(start,goal,mode="walking",departure_time=time_leaving)
-
-
-if directions_result:
-    route = directions_result[0]  # Get the first route, assuming it's the primary one
-    legs = route['legs']
-    print("Directions from {} to {}:".format(start, goal))
-    print("Distance: {}.".format(legs[0]['distance']['text']))
-    print("Duration: {}.".format(legs[0]['duration']['text']))
-    print("Departure Time: {}.".format(datetime.fromtimestamp(time_leaving).strftime('%Y-%m-%d %H:%M:%S')))
-    if 'arrival_time' in legs[0]:
-        print("Arrival Time: {}.".format(datetime.fromtimestamp(legs[0]['arrival_time']['value'])))
+if st.button("Get Directions"):
+    if time_leaving_input:
+        current_date = datetime.now()
+        hour, minute = map(int, time_leaving_input.split(":"))
+        time_leaving = datetime(current_date.year, current_date.month, current_date.day, hour, minute).timestamp()
     else:
-        print("Arrival Time information not available.")
-else:
-    print("No directions found.")
+        time_leaving = datetime.now().timestamp()
+
+    directions_result = gmaps.directions(start, goal, mode="walking", departure_time=time_leaving)
+
+    if directions_result:
+        route = directions_result[0]  # Get the first route, assuming it's the primary one
+        legs = route['legs']
+        st.write("**Journey from {} to {}:**".format(start, goal))
+        st.write("**Distance:** {}".format(legs[0]['distance']['text']))
+        st.write("**Duration:** {}".format(legs[0]['duration']['text']))
+        st.write("**Departure Time:** {}".format(datetime.fromtimestamp(time_leaving).strftime('%Y-%m-%d %H:%M:%S')))
+        if 'arrival_time' in legs[0]:
+            st.write("**Arrival Time:** {}".format(datetime.fromtimestamp(legs[0]['arrival_time']['value']).strftime('%Y-%m-%d %H:%M:%S')))
+    else:
+        st.write("No directions found.")
 

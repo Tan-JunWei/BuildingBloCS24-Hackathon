46c86f22ec1be05665a9258257c28f425bd9ab50
Ryan Low <ryan.cheeyang.low@gmail.com>
2024-06-12 00:07:45 +0800

    copied distance calc into main

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 import matplotlib.pyplot as plt
+import googlemaps
+from datetime import datetime
 
 st.set_page_config(page_title="Overview",page_icon="🔥")
 st.title("Ecobile")
@@ -41,3 +43,48 @@ with st.container():
             only reducing carbon emissions and traffic congestion but also fostering a more sustainable urban environemnt.")
     st.write("")
     st.write("However, more must be done.")
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+import streamlit as st
+import googlemaps
+from datetime import datetime
+
+# Streamlit interface
+st.title("Walking Directions Finder")
+
+api_key = st.text_input("Enter your Google Maps API key:")
+
+if not api_key:
+    st.warning("Please enter your Google Maps API key above.")
+    st.stop()
+
+gmaps = googlemaps.Client(key=api_key)
+
+start = st.text_input("Where are you starting your journey?")
+goal = st.text_input("Where would you like to go?")
+time_leaving_input = st.text_input("When will you be leaving? (HH\:MM) Leave blank if you are leaving now")
+
+if st.button("Get Directions"):
+    if time_leaving_input:
+        current_date = datetime.now()
+        hour, minute = map(int, time_leaving_input.split(":"))
+        time_leaving = datetime(current_date.year, current_date.month, current_date.day, hour, minute).timestamp()
+    else:
+        time_leaving = datetime.now().timestamp()
+
+    directions_result = gmaps.directions(start, goal, mode="walking", departure_time=time_leaving)
+
+    if directions_result:
+        route = directions_result[0]  # Get the first route, assuming it's the primary one
+        legs = route['legs']
+        st.markdown("**Directions from {} to {}:**".format(start, goal), unsafe_allow_html=True)  # Underline effect
+        st.write("**Distance:** {}".format(legs[0]['distance']['text']))
+        st.write("**Duration:** {}".format(legs[0]['duration']['text']))
+        st.write("**Departure Time:** {}".format(datetime.fromtimestamp(time_leaving).strftime('%Y-%m-%d %H:%M:%S')))
+        if 'arrival_time' in legs[0]:
+            st.write("**Arrival Time:** {}".format(datetime.fromtimestamp(legs[0]['arrival_time']['value']).strftime('%Y-%m-%d %H:%M:%S')))
+    else:
+        st.write("No directions found.")
+
+

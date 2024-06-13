6ddd64da4f8519c338c85f48a4ad548cbf1ab139
jwtan <jwtan0402@gmail.com>
2024-06-13 09:51:46 +0800

    fix historical emissions graph

--- main.py
+++ main.py
@@ -67,6 +67,38 @@ if page == "Overview":
     st.title("")
 
     st.header("Rise of Global Carbon Emissions")
+    carbon_emission_csv_path = 'historical_emissions.csv'
+
+    def load_data():
+        try:
+            data = pd.read_csv(carbon_emission_csv_path)
+            return data
+        except Exception as e:
+            st.error(f"Failed to load data: {e}")
+
+    data = load_data()
+
+    if data is not None:
+        countries = data['Country'].unique() #only unique countries
+
+        years = [int(col) for col in data.columns if col.isdigit()]
+
+        # Plot for each country
+        plt.figure(figsize=(15, 8))
+        for country in countries:
+            country_data = data[data['Country'] == country]
+            emissions = country_data.iloc[:, 5:].values.flatten()
+            plt.plot(years, emissions, label=country)
+
+        plt.title('CO2 Emissions by Country Over the Years')
+        plt.xlabel('Year')
+        plt.ylabel('CO2 Emissions (MtCO₂e)')
+        plt.xticks(years, rotation=45)
+        plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1),ncol=6)
+        plt.grid(True)
+        plt.tight_layout()
+
+        st.pyplot(plt)
     st.write("The trajectory of carbon emissions reveals an alarming exponential increase over time, casting a looming shadow over the fate of our\
              planet. It is imperative for us to acknowledge this and adopt strategies to curb this ascent before the devastating implications\
              of climate change become irreversible.")
